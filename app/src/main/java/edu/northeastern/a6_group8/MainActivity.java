c6a7d3464431d600b0834e6ce16d9cab2c4a1f06
Omkar Ubale <oubale@gmail.com>
2023-06-13 16:40:07 -0400

    SETUP: added WebServiceActivity for A6

--- app/src/main/java/edu/northeastern/a6_group8/MainActivity.java
+++ app/src/main/java/edu/northeastern/a6_group8/MainActivity.java
@@ -1,5 +1,7 @@
 package edu.northeastern.a6_group8;
 
+import android.content.Intent;
+import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +11,12 @@ public class MainActivity extends AppCompatActivity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+
+    Button aboutMeButton = findViewById(R.id.btn_web_service);
+
+    aboutMeButton.setOnClickListener(v -> {
+      Intent intent = new Intent(MainActivity.this, WebServiceActivity.class);
+      MainActivity.this.startActivity(intent);
+    });
   }
 }
