c97a00d528c677ce07bd83826d5b964369251f48
Omkar Ubale <oubale@gmail.com>
2023-06-16 17:21:27 -0400

    REFACTORING: cleaned up code in WebServiceActivity

--- app/src/main/java/edu/northeastern/NUMAD_23Su_Group8/WebServiceActivity.java
+++ app/src/main/java/edu/northeastern/NUMAD_23Su_Group8/WebServiceActivity.java
@@ -1,36 +1,63 @@
 package edu.northeastern.NUMAD_23Su_Group8;
 
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.os.Parcelable;
+import android.view.MenuItem;
+import android.view.View;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
 import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-import androidx.appcompat.widget.SearchView;
-
-import android.content.Intent;
-import android.graphics.Color;
-import android.net.Uri;
-import android.os.Bundle;
-import android.os.Parcelable;
-import android.util.Log;
-import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 import edu.northeastern.NUMAD_23Su_Group8.databinding.ActivityWebServiceBinding;
+import java.util.ArrayList;
+import java.util.List;
 
 public class WebServiceActivity extends AppCompatActivity {
 
 
   private ActivityWebServiceBinding binding;
-  RecyclerView weatherRecyclerView;
-  List<WeatherCard> weatherCardList;
-  List<WeatherCard> updatedWCList;
+
+  private Toolbar toolbar;
+  private SearchView weatherSearchView;
+
+  private RecyclerView weatherRecyclerView;
+  private WeatherAdapter weatherRecyclerViewAdapter;
+
+  private List<WeatherCard> weatherCardList;
+  private List<WeatherCard> updatedWCList;
 
   DummyDataGenerator dummyDataGenerator;
+
+  private void setupToolbar(ActivityWebServiceBinding binding) {
+    // setting toolbar with back button that navigates to the main page.
+    this.toolbar = binding.webServiceToolbar;
+
+    this.toolbar.setTitle("Web Service");
+    this.toolbar.setTitleTextColor(Color.WHITE);
+
+    setSupportActionBar(toolbar);
+    getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+  }
+
+  private void setupSearchView(ActivityWebServiceBinding binding) {
+    // expanding search bar so tap takes up entire area of the element (not .
+    this.weatherSearchView = binding.weatherSearchView;
+
+    this.weatherSearchView.onActionViewExpanded();
+    this.weatherSearchView.clearFocus();
+  }
+
+  private void setupRecyclerView(ActivityWebServiceBinding binding) {
+    // setting this as fixed for now, but if we need to adapt this later we can.
+    this.weatherRecyclerView = binding.weatherRecyclerView;
+
+    this.weatherRecyclerView.setHasFixedSize(true);
+    this.weatherRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+  }
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -44,35 +71,20 @@ public class WebServiceActivity extends AppCompatActivity {
     View view = binding.getRoot();
     setContentView(view);
 
-    // setting toolbar with back button that navigates to the main page.
-    Toolbar toolbar = binding.webServiceToolbar;
-    toolbar.setTitle("Web Service");
-    toolbar.setTitleTextColor(Color.WHITE);
-    setSupportActionBar(toolbar);
-    getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-
-    // expanding search bar so tap takes up entire area of the element (not .
-    binding.weatherSearchView.onActionViewExpanded();
-    binding.weatherSearchView.clearFocus();
-
     // instantiating the weatherCardList for the recyclerView
-    weatherCardList = new ArrayList<>();
+    this.weatherCardList = new ArrayList<>();
 
-    weatherRecyclerView = binding.weatherRecyclerView;
-
-    // setting this as fixed for now, but if we need to adapt this later we can.
-    weatherRecyclerView.setHasFixedSize(true);
-
-    weatherRecyclerView.setLayoutManager(new LinearLayoutManager(this));
-
+    this.setupToolbar(binding);
+    this.setupSearchView(binding);
+    this.setupRecyclerView(binding);
 
     // TODO substitute out the dummyData with real data from the API.
-    List<WeatherCard> dummyDataList = dummyDataGenerator.generateData(this);
+    List<WeatherCard> dummyDataList = DummyDataGenerator.generateData(this);
 
-    WeatherAdapter weatherAdapter = new WeatherAdapter(dummyDataList, this);
+    this.weatherRecyclerViewAdapter = new WeatherAdapter(dummyDataList, this);
 
-    // TODO subsitute this out with calls from API.
-    weatherCardList = dummyDataList;
+    // TODO substitute this out with calls from API.
+    this.weatherCardList = dummyDataList;
 
 
     /*
@@ -82,7 +94,7 @@ public class WebServiceActivity extends AppCompatActivity {
      * matches the appropriate cards to the user's search (based on locationName). Then, makes the
      * adapter's updateData call with the new list.
      */
-    binding.weatherSearchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+    this.weatherSearchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
       @Override
       public boolean onQueryTextSubmit(String query) {
         return false;
@@ -91,13 +103,13 @@ public class WebServiceActivity extends AppCompatActivity {
       @Override
       public boolean onQueryTextChange(String newText) {
 
-        updatedWCList = new ArrayList<>();
+        List<WeatherCard> updatedWCList = new ArrayList<>();
         for (WeatherCard card : weatherCardList) {
           if (card.getLocationName().toLowerCase().contains(newText.toLowerCase())) {
             updatedWCList.add(card);
           }
         }
-        weatherAdapter.updateData(updatedWCList);
+        WebServiceActivity.this.weatherRecyclerViewAdapter.updateData(updatedWCList);
         return true;
       }
     });
@@ -107,52 +119,55 @@ public class WebServiceActivity extends AppCompatActivity {
       @Override
       public void onSeeMoreClick(String city) {
         Intent intent = new Intent(WebServiceActivity.this, WeatherForecastDetailsActivity.class);
-        intent.putExtra("city",city);
+        intent.putExtra("city", city);
         startActivity(intent);
       }
     };
 
-    weatherAdapter.onCardListener(listener);
-    weatherRecyclerView.setAdapter(weatherAdapter);
+    this.weatherRecyclerViewAdapter.onCardListener(listener);
+    this.weatherRecyclerView.setAdapter(this.weatherRecyclerViewAdapter);
   }
 
   /**
    * Overriding the onOptionsItemSelected method in order to facilitate the navigation of the btn
    * from this page back to home.
-   * @param backButton The menu item that was selected.\
    *
-   * @return
+   * @param backButton The menu item that was selected.
+   * @return result from super.onOptionsItemSelected(backButton)
    */
   @Override
   public boolean onOptionsItemSelected(@NonNull MenuItem backButton) {
-
     finish();
     return super.onOptionsItemSelected(backButton);
   }
 
   /**
-   * This method is what saves the state of REcyclerVIew on screen rotation.
-   * @param state Bundle in which to place your saved state.
+   * This method is what saves the state of RecyclerView on screen rotation.
    *
+   * @param state Bundle in which to place your saved state.
    */
   @Override
-  protected void onSaveInstanceState(Bundle state) {
+  protected void onSaveInstanceState(@NonNull Bundle state) {
     super.onSaveInstanceState(state);
 
-    Parcelable recyclerState = weatherRecyclerView.getLayoutManager().onSaveInstanceState();
-    state.putParcelable("recyclerState", recyclerState);
+    if (this.weatherRecyclerView.getLayoutManager() != null) {
+      Parcelable recyclerState = this.weatherRecyclerView.getLayoutManager().onSaveInstanceState();
+      state.putParcelable("recyclerState", recyclerState);
+    }
   }
 
   /**
    * This method is what restores the saved state of recycler view on screen rotation.
-   * @param savedInstanceState the data most recently supplied in {@link #onSaveInstanceState}.
    *
+   * @param savedInstanceState the data most recently supplied in {@link #onSaveInstanceState}.
    */
   @Override
   protected void onRestoreInstanceState(Bundle savedInstanceState) {
     super.onRestoreInstanceState(savedInstanceState);
-    Parcelable recyclerState = savedInstanceState.getParcelable("recyclerState");
-    weatherRecyclerView.getLayoutManager().onRestoreInstanceState(recyclerState);
+    if (this.weatherRecyclerView.getLayoutManager() != null) {
+      Parcelable recyclerState = savedInstanceState.getParcelable("recyclerState");
+      this.weatherRecyclerView.getLayoutManager().onRestoreInstanceState(recyclerState);
+    }
 
   }
 }
