cbad54f7bb83cabba2075f99c911d1173fdf794a
PrajwalMathad <prajwalmathad966@gmail.com>
2023-06-29 12:17:39 +0530

    Added Activity for Messaging

--- app/src/main/java/edu/northeastern/NUMAD_23Su_Group8/MainActivity.java
+++ app/src/main/java/edu/northeastern/NUMAD_23Su_Group8/MainActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
+
+import edu.northeastern.NUMAD_23Su_Group8.Messaging.MessagingActivity;
 import edu.northeastern.NUMAD_23Su_Group8.Weather.WebServiceActivity;
 
 public class MainActivity extends AppCompatActivity {
@@ -14,9 +16,21 @@ public class MainActivity extends AppCompatActivity {
     setContentView(R.layout.activity_main);
 
     Button webServiceBtn = findViewById(R.id.btn_web_service);
+    Button aboutBtn = findViewById(R.id.about);
+    Button messagingBtn = findViewById(R.id.btn_messaging);
 
     webServiceBtn.setOnClickListener(v -> {
       Intent intent = new Intent(MainActivity.this, WebServiceActivity.class);
+      MainActivity.this.startActivity(intent);
+    });
+
+    aboutBtn.setOnClickListener(v -> {
+      Intent intent = new Intent(MainActivity.this, AboutUsActivity.class);
+      MainActivity.this.startActivity(intent);
+    });
+
+    messagingBtn.setOnClickListener(v -> {
+      Intent intent = new Intent(MainActivity.this, MessagingActivity.class);
       MainActivity.this.startActivity(intent);
     });
   }
---
+++ app/src/main/java/edu/northeastern/NUMAD_23Su_Group8/Messaging/MessagingActivity.java
@@ -0,0 +1,15 @@
+package edu.northeastern.NUMAD_23Su_Group8.Messaging;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import edu.northeastern.NUMAD_23Su_Group8.R;
+
+public class MessagingActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.messaging);
+    }
+}
