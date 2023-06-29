4dfe70f9aab0fcc8444ed6f00d3c06931ba2622e
PrajwalMathad <prajwalmathad966@gmail.com>
2023-06-29 11:43:42 +0530

    Added Activity for About us.

---
+++ app/src/main/java/edu/northeastern/NUMAD_23Su_Group8/AboutUsActivity.java
@@ -0,0 +1,13 @@
+package edu.northeastern.NUMAD_23Su_Group8;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AboutUsActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.about_us);
+    }
+}
